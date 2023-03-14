97d8b6f056a69e609ab153cee435f76f45be38ac
YumaKimura <y.kimura.0608@gmail.com>
2023-03-14 21:35:08 +0900

    モーダルの画面遷移を追加

--- App.tsx
+++ App.tsx
@@ -1,9 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const HomeStack = createStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <HomeStack.Navigator>
+        <HomeStack.Group>
+          <HomeStack.Screen name="Home" component={HomeScreen} />
+        </HomeStack.Group>
+        <HomeStack.Group
+          screenOptions={{
+            presentation: "modal",
+            headerMode: "float",
+            headerShown: false,
+          }}
+        >
+          <HomeStack.Screen name="Modal" component={ModalScreen} />
+        </HomeStack.Group>
+      </HomeStack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Home</Text>
+      <Button title="Open Modal" onPress={() => navigation.navigate("Modal")} />
+    </View>
+  );
+}
+
+function ModalScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Modal</Text>
+      <Button title="Close Modal" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -11,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
